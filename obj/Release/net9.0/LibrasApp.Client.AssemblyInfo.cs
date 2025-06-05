24bed0d987d99f4daed18154bf4cc2d424f089d8
Victor Guilherme <Victor.guilherme.silveira61@aluno.ifce.edu.br>
2025-06-05 00:11:12 -0300

    atualizando css

--- obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
+++ obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f20c46c17573ffe68d3933daaf265e7ab6659bf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17a3281a63392429d6dd4ac50e4b078ab6f962c9")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
