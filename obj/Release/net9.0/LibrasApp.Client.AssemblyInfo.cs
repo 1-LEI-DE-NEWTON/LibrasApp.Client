7129362f1748fe46a58a195f2296ffe4ae2e0db7
Victor Guilherme <Victor.guilherme.silveira61@aluno.ifce.edu.br>
2025-06-22 17:12:14 -0300

    publish q esqueci

--- obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
+++ obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+194095f962ff7b8013021068f4e3a3dff103b7b5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5caa54018dc28a63d0642fec5857db773fc46f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
