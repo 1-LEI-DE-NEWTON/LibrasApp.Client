d39d7b13abfd1f77a69ebf2c143fd9865761fedb
Victor Guilherme <Victor.guilherme.silveira61@aluno.ifce.edu.br>
2025-06-22 16:50:27 -0300

    Inclusao de parte 2: repolho roxo, até a reação + publish

--- obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
+++ obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ded0fae6d1db70b8684d1bd17c346f27b56d337")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4fa98168f37b29744513bcfadd335a8c9ee051")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
