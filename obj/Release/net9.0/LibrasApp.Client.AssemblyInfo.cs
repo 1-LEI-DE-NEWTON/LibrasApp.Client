576f509e0bd3fbdb016e357f2ecd3bc0dc83d206
Victor Guilherme <Victor.guilherme.silveira61@aluno.ifce.edu.br>
2025-06-23 01:20:24 -0300

    final publish lol

--- obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
+++ obj/Release/net9.0/LibrasApp.Client.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd8485a7ab2e715576220acad970374adf249de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7eddb743eee519640c476d9604c8027f4c82746")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibrasApp.Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
